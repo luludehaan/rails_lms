430b3b2324fc8293bfb5836815b3056411c0f9c9
LuLu DeHaan <luludehaan@gmail.com>
2021-03-09 17:45:44 -0800

    enrollment new

--- app/javascript/components/Course.js
+++ app/javascript/components/Course.js
@@ -1,9 +1,11 @@
 import React from 'react';
 const Course = ({ course }) => {
-  const { name } = course
+  const { name, id } = course
   return (
     <>
       <h1>Course: {name}</h1>
+      <hr />
+      <a href={`/courses/${id}/enrollments`}>Enrollments</a>
     </>
   )
 }
